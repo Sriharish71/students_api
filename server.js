8542049f884c024c7e22a1513ef80f8c514683fd
Sriharish71 <sriharish7106@gmail.com>
2025-10-16 09:04:29 +0530

    added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 const studentSchema = new mongoose.Schema({
     id:{
         type: String,
-        default: Date.now()
+        default:  () => Date.now().toString()
     },
     name:{
         type: String,
